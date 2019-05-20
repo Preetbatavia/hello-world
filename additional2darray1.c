b8695da968355e4878b83df49a18661cdd3fb9f9
Preetbatavia <50803985+Preetbatavia@users.noreply.github.com>
2019-05-20 22:24:31 +0530

    Add files via upload

---
+++ additional2darray1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+int m,n;
+printf("enter the vaalue of m:");
+scanf("%d",&m);
+printf("enter the vaalue of n:");
+scanf("%d",&n);
+
+int x[m][n];
+int i,j;
+printf("enter the value of the matrix:\n");
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
+scanf("%d",&x[i][j]);
+}
+for(i=0;i<m;i++)
+{int sum=0,sum1=0;
+for(j=0;j<n;j++)
+{
+sum=sum+x[i][j];
+sum1=sum1+x[j][i];
+}
+printf("sum of row %d is %d\n",i+1,sum);
+printf("sum of coloumn %d is %d\n",i+1,sum1);
+}
+
+
+
+
+
+
+}
